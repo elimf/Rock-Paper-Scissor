620fe924fa4eff8c918d8ac3571d0fb34da761f4
elimf <elim.florvil@icloud.com>
2021-11-15 22:07:31 +0100

    optimisation du jeu

--- test.js
+++ test.js
@@ -1,43 +1,100 @@
-function add7(b){
-  b +=7;
-  return b;
+function computerPlay() {
+  game = ["rock", "paper", "scissor"];
+  hand = game[Math.floor(Math.random() * game.length)];
+  console.log("Computer play " + hand);
+  return hand;
 }
+function playerSelection(name) {
+  var handle = window.prompt("What is your figure(rock,paper,scissor)");
+  handle = handle.toLowerCase();
 
-let c =add7(12)
-console.log(c);
-function multiply(a,b){
-  var produit = a*b;
-  return produit;
+  switch (handle) {
+    case "rock":
+      console.log(name + " chose " + handle);
+      break;
+    case "paper":
+      console.log(name + " chose " + handle);
+      break;
+    case "scissor":
+      console.log(name + " chose" + handle);
+      break;
+    case "exit":
+      console.log("Good bye");
+      window.stop;
+      break;
 
+    default:
+      console.log(name + "Your figure is incorrect.");
+      playerSelection(name);
+  }
+  return handle;
 }
-console.log(multiply(12, 2)); 
-function capitalize(string){
-
-  string = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase() ;
-  return string;
-
-} 
-console.log(capitalize("bonjour"));
-function last(string){
-  let nb = string.length;
-  string = string.slice(nb-1);
-  return string;
+function playRound(playerSelection, computerPlay, name) {
+  if (playerSelection == "rock" && computerPlay == "scissor") {
+    console.log(name + " win! Rock beats scissor.");
+  } else if (playerSelection == "rock" && computerPlay == "paper") {
+    console.log(name + " lose! Paper beats rock.");
+  } else if (playerSelection == "rock" && computerPlay == "rock") {
+    console.log("Egality!!!");
+  } else if (playerSelection == "paper" && computerPlay == "scissor") {
+    console.log(name + " lose!Scissor beats paper .");
+  } else if (playerSelection == "paper" && computerPlay == "rock") {
+    console.log(name + " win! Paper beats rock .");
+  } else if (playerSelection == "paper" && computerPlay == "paper") {
+    console.log("Egality!!!");
+  } else if (playerSelection == "scissor" && computerPlay == "paper") {
+    console.log(name + " win!Scissor beats paper");
+  } else if (playerSelection == "scissor" && computerPlay == "rock") {
+    console.log(name + " lose! Rock beats scissor");
+  } else if (playerSelection == "scissor" && computerPlay == "scissor") {
+    console.log("Egality");
+  }
 }
-console.log("La dernière lettre est "+last("bonjour"));
-const prompt = require("prompt-sync")();
-let answer = parseInt(
-  prompt("Please enter the number you would like to FizzBuzz up to: ")
-);
-
-for (let i = 1; i <= answer; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+function setPseudo() {
+  var handle = window.prompt("What is your pseudo");
+  console.log("Your pseudo is " + handle);
+  return handle;
+}
+function main() {
+  alert(" Welcome to Rock Paper Scissor");
+  var chose = window.prompt(
+    "Press 'p'to play a single party.\n Press 'q' to quit the game.\nPress 'm' to play a many party. "
+  );
+  console.log("Your pseudo is " + handle);
+  switch (chose) {
+    case "p":
+      console.log("The game will start");
+      game();
+      break;
+    case "m":
+      var howMany = parseInt(prompt("select the number of game you will play ?"));
+      manyGame(howMany);
+      break;
+    case "q":
+      console.log("Good bye");
+      window.stop;
+    default:
+      console.log("Try again");
+      main();
+      break;
+  }
+}
+function game() {
+  var pseudo = setPseudo();
+  var player = playerSelection(pseudo);
+  var computer = computerPlay();
+  var result = playRound(player, computer, pseudo);
+  console.log(result);
+}
+function manyGame(nb) {
+  pseudo = setPseudo();
+  nb;
+  for (let i = 0; i < nb; i++) {
+    player = playerSelection(pseudo);
+    computer = computerPlay();
+    result = playRound(player, computer, pseudo);
+    console.log(result);
   }
 }
 
+main();
